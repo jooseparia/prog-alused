fad52ee2089965d971cb8aa19773efbfc6d30734
Joosep Aria <joosep.aria@voco.ee>
2023-11-02 15:41:02 +0200

    print examples

---
+++ print/examples/ex1.py
@@ -0,0 +1,28 @@
+print("hello world!")
+
+print("Welcome")
+print("Welcome to school!")
+
+print("""Welcome
+to school""")
+
+print("Hello, I am your program!")
+name = input("What is your name: ")
+print(f"Hello {name}!")
+
+
+print("Hi",end=" ")
+print(f"{name}!")
+
+print("first", "second")
+print(1, 2)
+print("My age is", 18)
+print("My age is " + str(18))
+
+print(f"Hi {name}!")  # Hi {name}
+price_without_tax = 20
+print(f"Price with tax is {price_without_tax * 1.2}")
+# Price with tax is 24.0
+print("Sum: {1 + 2 + 3}")  # Sum: {1 + 2 + 3}
+print(f"Sum: {1 + 2 + 3}")  # Sum: 6
+
